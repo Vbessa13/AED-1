6ddb30746919a1cbaaa6e686f188c945e326c9be
Vitor Santini Bessa <vitorbessa13@gmail.com>
2022-05-12 02:40:48 -0300

    Update Exercício 07.C

--- Labs/Lab 01 - Revisao C/Exercício 07.C
+++ Labs/Lab 01 - Revisao C/Exercício 07.C
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//é solicitado uma explicação para o erro que ocorre referente a essa questão em comparação com a anterior, a variavel do tipo inteiro nao consegue guardar o valor
+//gerado pelo fatorial de numeros maiores que 12, portanto para corrigir o erro é necessário utilizar variáveis do tipo double como feito abaixo
+
 int main(){
   int n;
   double fat = 1;
