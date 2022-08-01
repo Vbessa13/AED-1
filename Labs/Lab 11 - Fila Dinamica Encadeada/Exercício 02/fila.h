bb16382687f2f4ac87dd635ec36abca0a2021dfc
Vitor Bessa <vitorbessa13@gmail.com>
2022-08-01 19:44:52 -0300

    Att

---
+++ Labs/Lab 11 - Fila Dinamica Encadeada/Exercício 02/fila.h
@@ -0,0 +1,14 @@
+#ifndef FILAESTSEQ_H_INCLUDED
+#define FILAESTSEQ_H_INCLUDED
+
+typedef struct no * Fila;
+
+Fila cria_fila();
+int fila_vazia(Fila f);
+int insere_fim(Fila *f, int elem);
+int remove_ini(Fila *f, int *elem);
+void print_fila(Fila f);
+int le_final(Fila *f);
+
+
+#endif // FILAESTSEQ_H_INCLUDED
